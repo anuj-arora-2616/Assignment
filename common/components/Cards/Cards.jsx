b21cb7fa701e68a2a735b20cd612f6d9f80e7eda
anuj arora <arora2616@gmail.com>
2020-10-25 15:34:39 +0530

    less changes

--- common/components/Cards/Cards.jsx
+++ common/components/Cards/Cards.jsx
@@ -2,7 +2,7 @@ import Card from './Card/Card';
 
 function Cards({ details = [] }) {
     const cards = details.map((detail) => (<Card {...detail} />))
-    return (<div className="Flex" style={{ flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
+    return (<div className="Flex" style={{ flexWrap: 'wrap', justifyContent: 'right' }}>
         {cards}
     </div>)
 }
